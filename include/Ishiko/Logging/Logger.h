5904c04d3a59944942a6f563c4101efce2aa1bdc
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-08-27 12:26:28 +0100

    Add files

---
+++ include/Ishiko/Logging/Logger.h
@@ -0,0 +1,22 @@
+/*
+    Copyright (c) 2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-cpp/logging/blob/main/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_CPP_LOGGING_LOGGER_H_
+#define _ISHIKO_CPP_LOGGING_LOGGER_H_
+
+namespace Ishiko
+{
+namespace Logging
+{
+
+class Logger
+{
+};
+
+}
+}
+
+#endif
