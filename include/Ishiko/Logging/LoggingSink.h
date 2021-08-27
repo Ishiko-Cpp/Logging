6dc75b4ad396c63a5155224d6cb869c599957ef2
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-08-27 12:56:24 +0100

    Add files

---
+++ include/Ishiko/Logging/LoggingSink.h
@@ -0,0 +1,10 @@
+/*
+    Copyright (c) 2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-cpp/logging/blob/main/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_CPP_LOGGING_LOGGINGSINK_H_
+#define _ISHIKO_CPP_LOGGING_LOGGINGSINK_H_
+
+#endif
