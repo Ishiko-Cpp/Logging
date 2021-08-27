3b494dfbbab4880b2a4c48cca723fe1e553fa4a8
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-08-27 11:42:01 +0100

    Updates

---
+++ include/Ishiko/Logging.h
@@ -0,0 +1,14 @@
+/*
+    Copyright (c) 2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-cpp/logging/blob/main/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_CPP_LOGGING_H_
+#define _ISHIKO_CPP_LOGGING_H_
+
+#include "Logging/LoggingMacros.h"
+
+#include "Logging/linkoptions.h"
+
+#endif
