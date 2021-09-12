58a3bd9351c24d9fa6137ed5f43f5e2e9eaf6b6e
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-09-12 15:53:45 +0100

    Updates

--- include/Ishiko/Logging/LoggingMacros.h
+++ include/Ishiko/Logging/LoggingMacros.h
@@ -7,6 +7,9 @@
 #ifndef _ISHIKO_CPP_LOGGING_LOGGINGMACROS_H_
 #define _ISHIKO_CPP_LOGGING_LOGGINGMACROS_H_
 
+#define ISHIKO_LOG_ERROR(message) logger.error(message);
+#define ISHIKO_LOG_WARNING(message) logger.warning(message);
 #define ISHIKO_LOG_INFO(message) logger.info(message);
+#define ISHIKO_LOG_TRACE(message) logger.trace(message);
 
 #endif
--- tests/src/LoggingMacrosTests.cpp
+++ tests/src/LoggingMacrosTests.cpp
@@ -16,7 +16,34 @@ using namespace Ishiko::Tests;
 LoggingMacrosTests::LoggingMacrosTests(const TestNumber& number, const TestEnvironment& environment)
     : TestSequence(number, "Logging macros tests", environment)
 {
+    append<HeapAllocationErrorsTest>("ISHIKO_LOG_ERROR test 1", IshikoLogErrorMacroTest1);
+    append<HeapAllocationErrorsTest>("ISHIKO_LOG_WARNING test 1", IshikoLogWarningMacroTest1);
     append<HeapAllocationErrorsTest>("ISHIKO_LOG_INFO test 1", IshikoLogInfoMacroTest1);
+    append<HeapAllocationErrorsTest>("ISHIKO_LOG_TRACE test 1", IshikoLogTraceMacroTest1);
+}
+
+void LoggingMacrosTests::IshikoLogErrorMacroTest1(Test& test)
+{
+    std::stringstream stream;
+    StreamLoggingSink sink(stream);
+    Logger logger(sink);
+
+    ISHIKO_LOG_ERROR("message");
+    
+    ISHIKO_FAIL_IF_NEQ(stream.str(), "message");
+    ISHIKO_PASS();
+}
+
+void LoggingMacrosTests::IshikoLogWarningMacroTest1(Test& test)
+{
+    std::stringstream stream;
+    StreamLoggingSink sink(stream);
+    Logger logger(sink);
+
+    ISHIKO_LOG_WARNING("message");
+
+    ISHIKO_FAIL_IF_NEQ(stream.str(), "message");
+    ISHIKO_PASS();
 }
 
 void LoggingMacrosTests::IshikoLogInfoMacroTest1(Test& test)
@@ -26,7 +53,19 @@ void LoggingMacrosTests::IshikoLogInfoMacroTest1(Test& test)
     Logger logger(sink);
 
     ISHIKO_LOG_INFO("message");
-    
+
+    ISHIKO_FAIL_IF_NEQ(stream.str(), "message");
+    ISHIKO_PASS();
+}
+
+void LoggingMacrosTests::IshikoLogTraceMacroTest1(Test& test)
+{
+    std::stringstream stream;
+    StreamLoggingSink sink(stream);
+    Logger logger(sink);
+
+    ISHIKO_LOG_TRACE("message");
+
     ISHIKO_FAIL_IF_NEQ(stream.str(), "message");
     ISHIKO_PASS();
 }
