033cf2f9d9562d69384cd6f999bc6e347cd4d8a4
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-04-10 17:13:14 +0100

    Update code after change to dependencies

--- tests/src/StreamLoggingSinkTests.hpp
+++ tests/src/StreamLoggingSinkTests.hpp
@@ -7,7 +7,7 @@
 #ifndef _ISHIKO_CPP_LOGGING_TESTS_STREAMLOGGINGSINKTESTS_HPP_
 #define _ISHIKO_CPP_LOGGING_TESTS_STREAMLOGGINGSINKTESTS_HPP_
 
-#include <Ishiko/Tests/Core.hpp>
+#include <Ishiko/TestFramework/Core.hpp>
 
 class StreamLoggingSinkTests : public Ishiko::TestSequence
 {
