3c7ef8f8c5f91447ef861b4c57d21f5a906a634a
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-08-02 23:58:15 +0100

    Update test code after changes to dependencies (#36)

--- tests/src/StandardStreamsLoggingSinkTests.cpp
+++ tests/src/StandardStreamsLoggingSinkTests.cpp
@@ -26,22 +26,20 @@ void StandardStreamsLoggingSinkTests::ConstructorTest1(Test& test)
 
 void StandardStreamsLoggingSinkTests::SendTest1(Test& test)
 {
+    const char* stdout_basename = "StandardStreamsLoggingSinkTests_SendTest1_out.txt";
+    const char* stderr_basename = "StandardStreamsLoggingSinkTests_SendTest1_err.txt";
+
+    CurrentProcess::RedirectStandardOutputToFile(test.context().getOutputPath(stdout_basename));
+    CurrentProcess::RedirectStandardErrorToFile(test.context().getOutputPath(stderr_basename));
+
     StandardStreamsLoggingSink sink;
-
-    CurrentProcess::RedirectStandardOutputToFile(
-        test.context().getTestOutputPath("StandardStreamsLoggingSinkTests_SendTest1_out.txt"));
-    CurrentProcess::RedirectStandardErrorToFile(
-        test.context().getTestOutputPath("StandardStreamsLoggingSinkTests_SendTest1_err.txt"));
-
     sink.send(LoggingSink::Record(LogLevel::info, "info message"));
     sink.send(LoggingSink::Record(LogLevel::error, "error message"));
 
     CurrentProcess::RedirectStandardOutputToTerminal();
     CurrentProcess::RedirectStandardErrorToTerminal();
 
-    ISHIKO_TEST_FAIL_IF_FILES_NEQ("StandardStreamsLoggingSinkTests_SendTest1_out.txt",
-        "StandardStreamsLoggingSinkTests_SendTest1_out.txt");
-    ISHIKO_TEST_FAIL_IF_FILES_NEQ("StandardStreamsLoggingSinkTests_SendTest1_err.txt",
-        "StandardStreamsLoggingSinkTests_SendTest1_err.txt");
+    ISHIKO_TEST_FAIL_IF_OUTPUT_AND_REFERENCE_FILES_NEQ(stdout_basename);
+    ISHIKO_TEST_FAIL_IF_OUTPUT_AND_REFERENCE_FILES_NEQ(stderr_basename);
     ISHIKO_TEST_PASS();
 }
